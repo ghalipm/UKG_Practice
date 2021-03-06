49c895b6aa53d2ad887e1076e51e2df1b510d705
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2021-03-06 15:29:29 -0500

    Project completed fully: all works well and Thread.sleep is removed.

--- src/test/java/com/libraryAutomation/stepDefinitions/US4_stepDefinitions.java
+++ src/test/java/com/libraryAutomation/stepDefinitions/US4_stepDefinitions.java
@@ -10,19 +10,17 @@ public class US4_stepDefinitions extends BasePage {
     UsersPage usersPage=new UsersPage();
 
     @Then("user can add users with all valid info")
-    public void user_can_add_users_with_all_valid_info() throws InterruptedException {
+    public void user_can_add_users_with_all_valid_info()  {
         // Write code here that turns the phrase above into concrete actions
         usersPage.addUser();
-        Thread.sleep(2000);
         usersPage.saveChanges();
     }
 
     @Then("user able to close the add user window with {string} button")
-    public void user_able_to_close_the_add_user_window_with_button(String string) throws InterruptedException {
+    public void user_able_to_close_the_add_user_window_with_button(String string)  {
         // Write code here that turns the phrase above into concrete actions
         usersPage.addUser();
         //usersPage.closeButton.click();
-        Thread.sleep(2000);
         usersPage.closeAddUser(); // this option also works the same way.
 
     }
